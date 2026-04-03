b3b376c6eeff87e270d6aae56eb6ef2450cd17ca
blgianini <blgianini@gmail.com>
2026-04-03 18:39:11 -0300

    feat: removendo o App Health

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/layout/Providers";
-import { AppHealth } from "@/components/system/AppHealth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="pt-BR" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="bg-background text-foreground antialiased">
         
-        <Providers><AppHealth /> {children}</Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
